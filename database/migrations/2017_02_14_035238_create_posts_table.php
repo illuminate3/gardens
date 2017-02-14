65f53905b0183677f6cca5b32cc42469e5514fbb
sahamilton <stephen@crescentcreative.com>
2017-02-13 20:45:32 -0800

    Remove foreign key constraint

--- database/migrations/2017_02_14_035238_create_posts_table.php
+++ database/migrations/2017_02_14_035238_create_posts_table.php
@@ -24,7 +24,7 @@ class CreatePostsTable extends Migration
             $table->string('meta_description');
             $table->string('meta_keywords');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            
         });
     }
 
