8bf4b2f172311dc56b4bfce6cf3ae6849afa1a98
Stephen Hamilton <stephen@crescentcreative.com>
2017-05-14 08:27:23 -0700

    updating layouts re sudose

--- resources/views/site/layouts/default.blade.php
+++ resources/views/site/layouts/default.blade.php
@@ -91,7 +91,8 @@
 		</div>
 		<!-- ./ container -->
 </div>
-@if (config('app.debug') && config('app.environment') != 'production')
+
+@if (config('app.debug') && config('app.env') == 'local')
     @include('sudosu::user-selector')
 @endif
 		<!-- the following div is needed to make a sticky footer -->
