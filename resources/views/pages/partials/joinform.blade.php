6aff20cd79e3bd2449df4da31a24c9656139f3e5
Stephen Hamilton <stephen@crescentcreative.com>
2017-06-10 18:48:59 -0700

    Working on join form

--- resources/views/pages/partials/joinform.blade.php
+++ resources/views/pages/partials/joinform.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group  {{$errors->first('firstname') ?  'has-error' :  ''}}">
 	<label for 'firstname' class='col-sm-2 control-label'>First Name:</label>
 	<div class="col-sm-10">
-		<input required type="text" name ='firstname' class="form-control" />
+		<input required type="text" name ='firstname' class="form-control" value="{{old('firstname')}}" />
 		<span class='error'>{{$errors->first('firstname')}}</span>
 	</div>
 </div>
@@ -13,7 +13,7 @@
 <div class="form-group  {{$errors->first('lastname') ?  'has-error' :  ''}}">
 <label for 'lastname' class='col-sm-2 control-label'>Last Name:</label>
 <div class="col-sm-10">
-<input required type="text" name ='lastname' class="form-control" />
+<input required type="text" name ='lastname' class="form-control" value="{{old('lastname')}}" />
 <span class='error'>{{$errors->first('lastname')}}</span>
 </div></div>
 
@@ -22,7 +22,7 @@
 
 <label for email' class='col-sm-2 control-label'>Email:</label>
 <div class="col-sm-10">
-<input required type='text' class='form-control' name='email' />
+<input required type='text' class='form-control' name='email' value="{{old('email')}}" />
 
 <span class="error">{{$errors->first('email')}}</span>
 </div></div>
@@ -33,7 +33,7 @@
 
 <label for phone' class='col-sm-2 control-label'>Phone:</label>
 <div class="col-sm-10">
-<input required type='text' class='form-control' name='phone' />
+<input required type='text' class='form-control' name='phone'  value="{{old('phone')}}"/>
 
 <span class="error">{{$errors->first('phone')}}</span>
 </div></div>
@@ -43,7 +43,7 @@
 
 <label for address' class='col-sm-2 control-label' >Petaluma Address:</label>
 <div class="col-sm-10">
-<input required type='text' class='form-control' name='address' />
+<input required type='text' class='form-control' name='address'  value="{{old('address')}}" />
 
 <span class="error">{{$errors->first('address')}}</span>
 </div></div>
@@ -53,7 +53,7 @@
 
 <label for comments' class='col-sm-2 control-label'>Your Message:</label>
 <div class="col-sm-10">
-<textarea class='form-control' name='comments'>
+<textarea class='form-control' name='comments'>{{old('comments')}}
 </textarea>
 
 <span class="error">{{$errors->first('comments')}}</span>
