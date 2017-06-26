2f4d4842c74bbeb72ac4e44523e160861761f4d4
sahamilton <stephen@crescentcreative.com>
2017-06-26 09:51:14 -0700

    Adding SMS

--- config/app.php
+++ config/app.php
@@ -183,7 +183,7 @@ return [
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Dionera\BeanstalkdUI\BeanstalkdUIServiceProvider::class,
         Laravel\Tinker\TinkerServiceProvider::class,
-        
+        SimpleSoftwareIO\SMS\SMSServiceProvider::class,
 
 
 
@@ -236,8 +236,9 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Bugsnag'=>Bugsnag\BugsnagLaravel\Facades\Bugsnag::class,
         'Form' => Collective\Html\FormFacade::class,
-      'Html' => Collective\Html\HtmlFacade::class,
-      'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'SMS' => SimpleSoftwareIO\SMS\FacadesSMS::class,
 
     ],
 
